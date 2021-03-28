b93de4377b12f3003f9177ef288bfec34d864a4f
Matrosken <piranya1399@gmail.com>
2021-03-28 20:13:08 +0400

    fff

---
+++ app/js/main.js
@@ -0,0 +1,71 @@
+var swiper = new Swiper('.shower-slider', {
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.main-pagination',
+        type: 'bullets',
+      },
+    centeredSlides: true,
+  });
+
+  var swiper1 = new Swiper('.toilet-slider', {
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.toilet-pagination',
+        type: 'bullets',
+      },
+    centeredSlides: true,
+  });
+
+  var swiper2 = new Swiper('.dinner-slider', {
+    slidesPerView: 'auto',
+    scrollbar: {
+        el: '.dinner-scrollbar',
+        hide: false,
+        draggable: true,
+        snapOnRelease: false,
+      },
+  });
+
+  var swiper3 = new Swiper('.jungle-slider', {
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.jungle-pagination',
+        type: 'bullets',
+      },
+    centeredSlides: true,
+  });
+
+  var swiper4 = new Swiper('.beach-slider', {
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.beach-pagination',
+        type: 'bullets',
+      },
+    centeredSlides: true,
+  });
+
+  var swiper5 = new Swiper('.beach2-slider', {
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.beach2-pagination',
+        type: 'bullets',
+      },
+    centeredSlides: true,
+  });
+
+  var swiper6 = new Swiper('.insta-slider', {
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.insta-pagination',
+        type: 'bullets',
+      },
+    centeredSlides: true,
+  });
+  var swiper7 = new Swiper('.sky-slider', {
+    slidesPerView: 'auto',
+    pagination: {
+        el: '.sky-pagination',
+        type: 'bullets',
+      },
+    centeredSlides: true,
+  });
